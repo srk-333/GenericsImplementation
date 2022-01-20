4b4f8e7c186b2248a210e4b3b348d29d2aee49ed
srk-333 <sauravmk333@gmail.com>
2022-01-20 14:17:12 +0530

    [Saurav]Add: Find Max value from String type values

--- GenericsImplementation/FindMaximum/FindMaxValue.cs
+++ GenericsImplementation/FindMaximum/FindMaxValue.cs
@@ -8,8 +8,8 @@ namespace GenericsImplementation.FindMaximum
 {
     internal class FindMaxValue
     {
-        /*Method to check Max number out of Three number
-         * If any two or all numbers are same then return zero.
+        /*Method to check Max number out of Three number for Int data type vales.
+         *If any two or all numbers are same then return zero.
          */
         public int FindMaxInteger(int fisrtNum, int secondNum, int thirdNum)
         {
@@ -38,7 +38,7 @@ namespace GenericsImplementation.FindMaximum
             }
         }
 
-        //Mehod for Double type data value
+        //Method to check Max number out of Three number for double data type vales.
         public double FindMaxDouble(double first, double second, double third)
         {
             //check condition for max number using CompareTo method.
@@ -63,6 +63,34 @@ namespace GenericsImplementation.FindMaximum
             {
                 Console.WriteLine("two or more numbers are same");
                 return 0;
+            }
+        }
+
+        //Method to check Max value out of Three values for String data type vales.
+        public string FindMaxString(string first, string second, string third)
+        {
+            //check condition for max value using CompareTo method.
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("Max Value is First Value: " + first);
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("Max Value is Second Value: " + second);
+                return second;
+            }
+
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("Max Value is Third Value: " + third);
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("two or more numbers are same");
+                return "null";
             }
         }
     }
--- GenericsImplementation/Program.cs
+++ GenericsImplementation/Program.cs
@@ -14,18 +14,21 @@ namespace GenericsImplementation
             //Instanse variables.
             int first = 20, second = 300, third = 40;
             double f = 40.5, s = 60.6, t = 35.79;
+            string firstName = "saurav", secondName = "Ritu", thirdName = "Ankit";
 
             //Creating object of FindMaxValueClass.
             FindMaxValue findMaxValue = new FindMaxValue();          
-            Console.WriteLine("Max Integer number is:{0} out of numbers : {1} , {2} , {3} ", findMaxValue.FindMaxInteger(first, second, third),first,second,third +"\n");
+            Console.WriteLine("Max Integer number is:{0} out of numbers : {1} , {2} , {3} ", findMaxValue.FindMaxInteger(first, second, third), first, second, third +"\n");
             Console.WriteLine("Max double number is:{0} out of numbers : {1} , {2} , {3} ", findMaxValue.FindMaxDouble(f, s, t), f, s, t +"\n");
+            Console.WriteLine("Max String Value is:{0} out of values : {1} , {2} , {3} ", findMaxValue.FindMaxString(firstName, secondName, thirdName), firstName, secondName, thirdName + "\n");
 
             //Call GenericsInvoke Method
-           // Program p = new Program();
-           // p.GenericsInvoke();
+            //Program p = new Program();
+            //p.GenericsInvoke();
             Console.ReadLine();
         }
 
+        //Method to Invoke All Generics Related classes.
         public void GenericsInvoke()
         {
             //creating Array for diff data Tpes and assigining their values.
